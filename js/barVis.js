6d4b575a681e82ee0e6cd863a01d986eaed30832
Tucker Gordon <tucker.gordon18@gmail.com>
2016-10-27 09:22:10 -0400

    .js files can now read in dataset

---
+++ js/barVis.js
@@ -0,0 +1,14 @@
+var dataset = [];
+
+d3.csv("students.csv", function(error, data) {
+	dataset = data;         // copy to dataset
+
+    // error checking
+    if (error) {
+		console.log(error)
+    }
+    else {
+		console.log(data[681]["age"])	//just testing that it works
+		//createVis()
+    }
+});
